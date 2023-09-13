cf2715f46128f9b729f8591ea6d040e66ad97c55
ilgar789 <ilgar789@gmail.com>
2023-09-14 03:20:44 +0400

    Add_Tasks

---
+++ Task64/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void PrintNumbersFromNTo1(int n)
+    {
+        if (n < 1)
+        {
+            return;
+        }
+        else
+        {
+            Console.Write(n);
+            if (n != 1)
+            {
+                Console.Write(", ");
+            }
+            PrintNumbersFromNTo1(n - 1);
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.Write("Числа от " + N + " до 1: ");
+        PrintNumbersFromNTo1(N);
+
+        Console.WriteLine(); // Для перехода на новую строку в конце вывода.
+    }
+}
---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for (int i = M; i <= N; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine("Сумма натуральных чисел от " + M + " до " + N + " равна " + sum);
+    }
+}
